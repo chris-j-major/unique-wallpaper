7dcc28c68c25711d28d28812c5ee9a6cdc0b6cde
Chris M <chris.j.major@gmail.com>
2016-03-03 09:40:56 +0000

    colour tweaks

--- parts/palletes/midToneHilight.js
+++ parts/palletes/midToneHilight.js
@@ -12,7 +12,7 @@ function MidToneHilights( struct , stack ){
   var midHue = struct.random.range( 0 , 1.0 ); // randomHue
   var midLightness = (struct.random.bool()?0.3:0.7)+struct.random.range( -0.2 , 0.2 );
   var midSaturation = struct.random.range( 0.1 , 0.3 );
-  var highlightHue = midHue + struct.random.range( -0.1 , 0.1 )+(struct.random.bool()?0.5:0.0); // random ish Hue
+  var highlightHue = midHue + struct.random.range( -0.1 , 0.1 )+(1/3); // random ish Hue
   var highLightness = 0.5+struct.random.range( -0.1 , 0.1 );
   this.create();
   this.addRange( 0.4 , new Color(midHue,1*midSaturation,1*midLightness) );
