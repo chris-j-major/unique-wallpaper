228e692e3da44f208fd32ec5413617fa5a60d2d7
Chris M <chris.j.major@gmail.com>
2016-03-13 14:12:29 +0000

    Added grid pointset

---
+++ parts/lines/scribble.js
@@ -0,0 +1,36 @@
+var Part = require("../Part");
+
+module.exports =  [1,5,12].map(function(width){
+  return new Part(
+    "scribble("+width+")", /* name */
+    ["line","stright-line"], /* types */
+    function(){
+      this.color = this.opts.color || this.choseColor();
+      this.a = this.opts.a || {
+        x:this.random.range(0, this.opts.width ),
+        y:this.random.range(0, this.opts.height ) };
+      this.pointSet = this.createPart("pointset" , 12 , this.opts ),
+      this.subparts = [
+        this.pointSet
+      ];
+      this.length = this.random.range(2,20);
+      this.description="("+this.a.x+","+this.a.y+"),"+this.color.toHex();
+    },
+    {
+      buildXML:function(xml){
+        var path = "M"+this.a.x+" "+this.a.y;
+        for ( var i=0 ;i<this.length;i++){
+          var p = this.pointSet.generate(i*2);
+          var q = this.pointSet.generate((i*2)+1);
+          path+= " Q"+p.x+" "+p.y+" "+q.x+" "+q.y
+        }
+        xml.ele('path',{
+          d:path,
+          stroke:this.color.toHex(),
+          'stroke-width':width,
+          fill:"none"
+        });
+      }
+    }
+  );
+})
---
+++ parts/pointset/grid.js
@@ -0,0 +1,35 @@
+var Part = require("../Part");
+
+module.exports = new Part(
+  "linear", /* name */
+  ["pointset","pointset-dynamic"], /* types */
+  function(){
+    this.x = this.random.range(0, this.opts.width );
+    this.y = this.random.range(0, this.opts.height );
+    this.x2 = this.random.range(0, this.opts.width );
+    this.y2 = this.random.range(0, this.opts.height );
+    this.dx1 = (this.x2 - this.x) / (this.opts.range||20);
+    this.dy1 = (this.y2 - this.y) / (this.opts.range||20);
+    this.x3 = this.random.range(0, this.opts.width );
+    this.y3 = this.random.range(0, this.opts.height );
+    this.dx2 = (this.x3 - this.x) / (this.opts.range||20);
+    this.dy2 = (this.y3 - this.y) / (this.opts.range||20);
+    this.description = this.x+"+"+this.dy+"*i"+this.dx2+"*j,"+this.y+"+"+this.dy+"*i"+this.dy2+"*j"
+  },
+  {
+    generate:function(id){
+    var n = Math.floor(Math.sqrt(id));
+    var m = id - (n*n);
+    var i = 0;
+    var j = 0;
+    if ( m <= n ){
+      i = n;
+      j = m;
+    }else{
+      i = (2*n)-m;
+      j = n;
+    }
+      return {x:this.x+(this.dx1*i)+(this.dx2*j),y:this.y+(this.dy1*i)+(this.dy2*j)}
+    }
+  }
+);
